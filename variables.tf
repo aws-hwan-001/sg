d31c9dbb48cc8f8d5d60a5bed1614df77be95eb4
hwan001 <hwan001@interpark.com>
2023-07-29 20:50:11 +0900

    init

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "description" {
+  description = "Description of the security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID to associate the security group with"
+  type        = string
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules"
+  type        = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "name" {
+  description = "Name of the security group"
+  type        = string
+}
